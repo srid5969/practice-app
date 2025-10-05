92ea69c382c12c3b393cc774f31293abc51a6a96
srid5969 <sridharofficialmail@gmail.com>
2025-10-05 09:43:34 +0530

    Implement name input handling and validation in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
 
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+  const [name, setName] = useState<string | undefined>(undefined)
 
   return (
     <>
       <h1>Welcome</h1>
       <div>
         <label>
-         Please Enter Your Name :
-          <input type="text" name="username" />
+          Please Enter Your Name :
+          <input
+            type="text"
+            name="username"
+            value={name ?? ''}
+            onChange={e => setName(e.target.value)}
+          />
         </label>
-     
+
       </div>
       <button
         onClick={() => {
-          alert('Submit button clicked');
+          if (!name) {
+            alert('Kindly enter your name');
+            return;
+          }
+
+          alert(`Vanakkam ${name} , 
+            Ungalai Sridhar Anbudan varaverkiraar`);
         }}
       >Submit</button>
     </>
