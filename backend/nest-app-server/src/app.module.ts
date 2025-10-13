9bce6197f2d85fdb0e80cd7c04ad42e7d49333be
srid5969 <sridharofficialmail@gmail.com>
2025-10-13 13:37:54 +0530

    feat: initialize NestJS application with basic structure and configuration
    - Added package.json for NestJS application setup
    - Created AppController and AppService with a simple "Hello World!" endpoint
    - Implemented AppModule to configure application modules and MongoDB connection
    - Added BoardModule and ColumnsModule as placeholders for future development
    - Defined schemas for Board, Column, and Task entities
    - Configured application settings in a centralized config file
    - Set up main entry point for the application
    - Added Jest testing framework with initial unit and e2e tests
    - Created TypeScript configuration files for building and development

---
+++ backend/nest-app-server/src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { config } from './configuration/config';
+import { BoardModule } from './board/board.module';
+import { TaskModule } from './task/task.module';
+import { ColumnsModule } from './columns/columns.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.getOrThrow<string>('database.uri'),
+        dbName: configService.getOrThrow<string>('database.name'),
+      }),
+    }),
+    BoardModule,
+    TaskModule,
+    ColumnsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
---
+++ backend/nest-app-server/src/task/schemas/tasks.schema.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Types } from 'mongoose';
+
+@Schema({ timestamps: true })
+export class Task  {
+  @Prop({ required: true })
+  title: string;
+
+  @Prop()
+  description: string;
+
+  @Prop({ default: 0 })
+  order: number; // for sorting inside a column
+
+  @Prop({ type: Types.ObjectId, ref: 'Column', required: true })
+  columnId: Types.ObjectId;
+
+  @Prop({ type: Types.ObjectId, ref: 'Board', required: true })
+  boardId: Types.ObjectId;
+}
+
+export const TaskSchema = SchemaFactory.createForClass(Task);
