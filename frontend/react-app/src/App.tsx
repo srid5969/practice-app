e1bbc757391d44afb61ce343f94d86be3ed84b72
srid5969 <sridharofficialmail@gmail.com>
2025-10-13 14:39:11 +0530

    feat: add BoardsPage component and API service for board management; update routing and configurations

--- frontend/react-app/src/App.tsx
+++ frontend/react-app/src/App.tsx
@@ -3,40 +3,12 @@ import { useState } from 'react';
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import Dashboard from './pages/dashboard';
+import BoardsPage from './pages/boards';
 
 function Home() {
-  const [name, setName] = useState<string | undefined>(undefined)
   const navigate = useNavigate();
-
-  return (
-    <>
-      <h1>Welcome</h1>
-      <div>
-        <label>
-          Please Enter Your Name :
-          <input
-            type="text"
-            name="username"
-            value={name ?? ''}
-            onChange={e => setName(e.target.value)}
-          />
-        </label>
-
-      </div>
-      <button
-        onClick={() => {
-          if (!name) {
-            alert('Kindly enter your name');
-            return;
-          }
-
-          alert(`Vanakkam ${name} , 
-            Ungalai Sridhar Anbudan varaverkiraar`);
-            navigate('/dashboard');
-        }}
-      >Submit</button>
-    </>
-  )
+  navigate('/boards');
+  return <div>Redirecting to Boards...</div>;
 }
 
 function App(){
@@ -44,7 +16,7 @@ function App(){
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/boards" element={<BoardsPage />} />
       </Routes>
     </Router>
   );
